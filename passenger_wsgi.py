a6ffafce5724fe23234801d6b6bf4f3d11b5ad88
AG <gorelikspb@ya.ru>
2019-02-09 12:02:14 +0300

    wsgi back2

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,8 @@
+-*- coding: utf-8 -*-
 
-import os, sys
+ import os, sys
 
 #project directory
-
-sys.path.insert(0, '<полный_путь_до_каталога_с_проектом>')
-sys.path.insert(1, '<полный_путь_до_Django>')
-
 sys.path.append('/home/d/drimspb/.local/lib/python2.7/site-packages')
 sys.path.append('/home/d/drimspb/drugoekino.top/public_html/mysite')
 
@@ -17,6 +14,28 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
+
+
+
+#
+# import os, sys
+#
+# #project directory
+#
+# sys.path.insert(0, '<полный_путь_до_каталога_с_проектом>')
+# sys.path.insert(1, '<полный_путь_до_Django>')
+#
+# sys.path.append('/home/d/drimspb/.local/lib/python2.7/site-packages')
+# sys.path.append('/home/d/drimspb/drugoekino.top/public_html/mysite')
+#
+# #project settings
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#
+# #start server
+#
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+#
 
 
 #==это работало но не обновлялось
