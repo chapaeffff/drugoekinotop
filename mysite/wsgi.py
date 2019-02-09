0a1b7eab6498f88ff568f1c6e082240cb275fb4b
AG <gorelikspb@ya.ru>
2019-02-09 11:53:21 +0300

    wsgi back

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,14 +1,72 @@
-# -*- coding: utf-8 -*-
+-*- coding: utf-8 -*-
 
-import os,sys
 
-#путь к проекту
-sys.path.insert(0, '/home/d/drimspbru/drugoekinotop/public_html')
-#путь к фреймворку
-sys.path.insert(0, '/home/d/drimspbru/drugoekinotop')
-#путь к виртуальному окружению
-sys.path.insert(0, '/home/d/drimspbru/.djangovenv/lib64/python2.7/site-packages/')
-os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
+import os, sys
+
+#project directory
+sys.path.append('/home/d/drimspb/.local/lib/python2.7/site-packages')
+sys.path.append('/home/d/drimspb/drugoekino.top/public_html/mysite')
+
+#project settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+#start server
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+
+# import os,sys
+#
+# #путь к проекту
+# sys.path.insert(0, '/home/d/drimspbru/drugoekinotop/public_html')
+# #путь к фреймворку
+# sys.path.insert(0, '/home/d/drimspbru/drugoekinotop')
+# #путь к виртуальному окружению
+# sys.path.insert(0, '/home/d/drimspbru/.djangovenv/lib64/python2.7/site-packages/')
+# os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
+#
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+#
+# #
+# старое
+#
+#
+# import os, sys
+#
+# #project directory
+#
+# sys.path.insert(0, '<полный_путь_до_каталога_с_проектом>')
+# sys.path.insert(1, '<полный_путь_до_Django>')
+#
+# sys.path.append('/home/d/drimspb/.local/lib/python2.7/site-packages')
+# sys.path.append('/home/d/drimspb/drugoekino.top/public_html/mysite')
+#
+# #project settings
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#
+# #start server
+#
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+#
+#
+#
+# #==это работало но не обновлялось
+#
+# # import os, sys
+# #
+# # #project directory
+# # sys.path.append('/home/d/drimspb/.local/lib/python2.7/site-packages')
+# # sys.path.append('/home/d/drimspb/drugoekino.top/public_html/mysite')
+# #
+# # #project settings
+# # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# #
+# # #start server
+# #
+# # from django.core.wsgi import get_wsgi_application
+# # application = get_wsgi_application()
+#
