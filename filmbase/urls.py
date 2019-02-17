f8cfba6ddb65b57ea4cb50ffbf75c0cf12d64e5a
AG <gorelikspb@ya.ru>
2019-02-17 16:55:18 +0300

    working on add movie

--- filmbase/urls.py
+++ filmbase/urls.py
@@ -1,12 +1,14 @@
 #from django.urls import include, path
 from django.conf.urls import include, url
 
-
 from . import views
 
 app_name = 'filmbase'
 
 urlpatterns = [
+    # url(r'^/$', views.searchkp, name='searchkp'),
     url('(?P<pk>\d+)/$', views.film_detail, name='film_detail'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^searching/$', views.searching, name='searching'),
 
 ]
--- filmbase/views.py
+++ filmbase/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.http import HttpResponse
+from kinopoisk.movie import Movie
 
 from blog.models import *
 from video.models import *
@@ -21,3 +22,45 @@ def film_detail(request, pk):
     return render(request, 'filmbase/film_detail.html', {'film': film, 'videos': videos,
                                                      'from_lists': from_lists})
     # return render(request, 'filmbase/film_detail.html', {'pk': pk})
+
+
+
+
+
+
+def search(request):
+    print('search')
+    # # return render(request, 'index/index.html')
+    # if 'q' in request.GET:
+    #     query_string = request.GET['q']
+    #     print (query_string)
+    return render(request, 'filmbase/search.html')
+
+
+def searching(request):
+    if 'q' in request.GET:
+        query_string = request.GET['q']
+        # message = "You searched for: %r" % request.GET['q']
+        movie_list = Movie.objects.search(query_string)
+        m = movie_list[0]
+        # print (vars(m))
+        # f = Movie(id = m.id)
+        #
+        # print (vars(f))
+        #
+        message = m.title
+
+        google_kp(message)
+        #
+        # movie_list = Movie.objects.search('Redacted')
+        # print(movie_list[0].id)
+
+        # m = Movie(id=160977)
+        # m.get_content('main_page')
+        # print(m.title)  # вывод: Апокалипсис
+        # print(m.directors)  # вывод: []
+
+    else:
+        message = "You submitted an empty form."
+    return HttpResponse(message)
+
