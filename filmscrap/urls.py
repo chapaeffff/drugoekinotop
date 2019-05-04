d88b9def6494b69e772ad5359b3c1504f35b86f1
AG <gorelikspb@ya.ru>
2019-05-04 17:34:45 +0300

    ruls2

--- filmscrap/urls.py
+++ filmscrap/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from django.urls import path
-
 from . import views
 
 # addfilm_url = reverse('admin:filmbase_film_add')
